009f509d13d8164a8b44eec8d1a3199a7fc43813
jankaminski <jan.kaminski665@gmail.com>
2025-01-23 13:39:10 +0100

    Refactored and simplified matrices in math utils.

--- math-utils.js
+++ math-utils.js
@@ -98,7 +98,7 @@ class Mat4 {
     }
     static perspective(fovy, width, height, near, far) {
         let out = new Float32Array(16);
-        var f = 1.0 / Math.tan(fovy / 2);
+        let f = 1.0 / Math.tan(fovy / 2);
         out[0] = f / (width / height);
         out[1] = 0;
         out[2] = 0;
@@ -114,7 +114,7 @@ class Mat4 {
         out[13] = 0;
         out[15] = 0;
         if (far != null && far !== Infinity) {
-            var nf = 1 / (near - far);
+            let nf = 1 / (near - far);
             out[10] = (far + near) * nf;
             out[14] = 2 * far * near * nf;
         } else {
@@ -124,107 +124,66 @@ class Mat4 {
         return out;
     }
     static translate(a, v) {
-        var x = v.x,
+        let x = v.x,
             y = v.y,
             z = v.z;
-        var a00, a01, a02, a03;
-        var a10, a11, a12, a13;
-        var a20, a21, a22, a23;
-        let out = new Float32Array(16);
-        if (a === out) {
-            out[12] = a[0] * x + a[4] * y + a[8] * z + a[12];
-            out[13] = a[1] * x + a[5] * y + a[9] * z + a[13];
-            out[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
-            out[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
-        } else {
-            a00 = a[0];
-            a01 = a[1];
-            a02 = a[2];
-            a03 = a[3];
-            a10 = a[4];
-            a11 = a[5];
-            a12 = a[6];
-            a13 = a[7];
-            a20 = a[8];
-            a21 = a[9];
-            a22 = a[10];
-            a23 = a[11];   
-            out[0] = a00;
-            out[1] = a01;
-            out[2] = a02;
-            out[3] = a03;
-            out[4] = a10;
-            out[5] = a11;
-            out[6] = a12;
-            out[7] = a13;
-            out[8] = a20;
-            out[9] = a21;
-            out[10] = a22;
-            out[11] = a23;
-            out[12] = a00 * x + a10 * y + a20 * z + a[12];
-            out[13] = a01 * x + a11 * y + a21 * z + a[13];
-            out[14] = a02 * x + a12 * y + a22 * z + a[14];
-            out[15] = a03 * x + a13 * y + a23 * z + a[15];
-        }
+        let out = new Float32Array(16);  
+        out[0] = a[0];
+        out[1] = a[1];
+        out[2] = a[2];
+        out[3] = a[3];
+        out[4] = a[4];
+        out[5] = a[5];
+        out[6] = a[6];
+        out[7] = a[7];
+        out[8] = a[8];
+        out[9] = a[9];
+        out[10] = a[10];
+        out[11] = a[11];
+        out[12] = a[0] * x + a[4] * y + a[8]  * z + a[12];
+        out[13] = a[1] * x + a[5] * y + a[9]  * z + a[13];
+        out[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
+        out[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
         return out;
     }
     static EPSILON = 0.000001;
     static rotate(a, rad, axis) {
-        var x = axis[0],
+        let x = axis[0],
             y = axis[1],
             z = axis[2];
-        var len = Math.hypot(x, y, z);
-        var s, c, t;
-        var a00, a01, a02, a03;
-        var a10, a11, a12, a13;
-        var a20, a21, a22, a23;
-        var b00, b01, b02;
-        var b10, b11, b12;
-        var b20, b21, b22;
-        if (len < Mat4.EPSILON) {
+        let len = Math.hypot(x, y, z);
+        if (len < Mat4.EPSILON)
             return null;
-        }
         len = 1 / len;
         x *= len;
         y *= len;
         z *= len;
-        s = Math.sin(rad);
-        c = Math.cos(rad);
-        t = 1 - c;
-        a00 = a[0];
-        a01 = a[1];
-        a02 = a[2];
-        a03 = a[3];
-        a10 = a[4];
-        a11 = a[5];
-        a12 = a[6];
-        a13 = a[7];
-        a20 = a[8];
-        a21 = a[9];
-        a22 = a[10];
-        a23 = a[11]; // Construct the elements of the rotation matrix
-        b00 = x * x * t + c;
-        b01 = y * x * t + z * s;
-        b02 = z * x * t - y * s;
-        b10 = x * y * t - z * s;
-        b11 = y * y * t + c;
-        b12 = z * y * t + x * s;
-        b20 = x * z * t + y * s;
-        b21 = y * z * t - x * s;
-        b22 = z * z * t + c; // Perform rotation-specific matrix multiplication
+        let s = Math.sin(rad);
+        let c = Math.cos(rad);
+        let t = 1 - c;
+        let rotComponent = new Float32Array(9);
+        rotComponent[0] = x * x * t + c;
+        rotComponent[1] = y * x * t + z * s;
+        rotComponent[2] = z * x * t - y * s;
+        rotComponent[3] = x * y * t - z * s;
+        rotComponent[4] = y * y * t + c;
+        rotComponent[5] = z * y * t + x * s;
+        rotComponent[6] = x * z * t + y * s;
+        rotComponent[7] = y * z * t - x * s;
+        rotComponent[8] = z * z * t + c; 
         let out = new Float32Array(16);
-        out[0] = a00 * b00 + a10 * b01 + a20 * b02;
-        out[1] = a01 * b00 + a11 * b01 + a21 * b02;
-        out[2] = a02 * b00 + a12 * b01 + a22 * b02;
-        out[3] = a03 * b00 + a13 * b01 + a23 * b02;
-        out[4] = a00 * b10 + a10 * b11 + a20 * b12;
-        out[5] = a01 * b10 + a11 * b11 + a21 * b12;
-        out[6] = a02 * b10 + a12 * b11 + a22 * b12;
-        out[7] = a03 * b10 + a13 * b11 + a23 * b12;
-        out[8] = a00 * b20 + a10 * b21 + a20 * b22;
-        out[9] = a01 * b20 + a11 * b21 + a21 * b22;
-        out[10] = a02 * b20 + a12 * b21 + a22 * b22;
-        out[11] = a03 * b20 + a13 * b21 + a23 * b22;
+        out[0] =  a[0] * rotComponent[0] + a[4] * rotComponent[1] + a[8]  * rotComponent[2];
+        out[1] =  a[1] * rotComponent[0] + a[5] * rotComponent[1] + a[9]  * rotComponent[2];
+        out[2] =  a[2] * rotComponent[0] + a[6] * rotComponent[1] + a[10] * rotComponent[2];
+        out[3] =  a[3] * rotComponent[0] + a[7] * rotComponent[1] + a[11] * rotComponent[2];
+        out[4] =  a[0] * rotComponent[3] + a[4] * rotComponent[4] + a[8]  * rotComponent[5];
+        out[5] =  a[1] * rotComponent[3] + a[5] * rotComponent[4] + a[9]  * rotComponent[5];
+        out[6] =  a[2] * rotComponent[3] + a[6] * rotComponent[4] + a[10] * rotComponent[5];
+        out[7] =  a[3] * rotComponent[3] + a[7] * rotComponent[4] + a[11] * rotComponent[5];
+        out[8] =  a[0] * rotComponent[6] + a[4] * rotComponent[7] + a[8]  * rotComponent[8];
+        out[9] =  a[1] * rotComponent[6] + a[5] * rotComponent[7] + a[9]  * rotComponent[8];
+        out[10] = a[2] * rotComponent[6] + a[6] * rotComponent[7] + a[10] * rotComponent[8];
+        out[11] = a[3] * rotComponent[6] + a[7] * rotComponent[7] + a[11] * rotComponent[8];
         out[12] = a[12];
         out[13] = a[13];
         out[14] = a[14];
@@ -232,20 +191,20 @@ class Mat4 {
         return out;
     }
     static scale(a, v) {
-        var x = v.x,
+        let x = v.x,
             y = v.y,
             z = v.z;
         let out = new Float32Array(16);
-        out[0] = a[0] * x;
-        out[1] = a[1] * x;
-        out[2] = a[2] * x;
-        out[3] = a[3] * x;
-        out[4] = a[4] * y;
-        out[5] = a[5] * y;
-        out[6] = a[6] * y;
-        out[7] = a[7] * y;
-        out[8] = a[8] * z;
-        out[9] = a[9] * z;
+        out[0] =  a[0]  * x;
+        out[1] =  a[1]  * x;
+        out[2] =  a[2]  * x;
+        out[3] =  a[3]  * x;
+        out[4] =  a[4]  * y;
+        out[5] =  a[5]  * y;
+        out[6] =  a[6]  * y;
+        out[7] =  a[7]  * y;
+        out[8] =  a[8]  * z;
+        out[9] =  a[9]  * z;
         out[10] = a[10] * z;
         out[11] = a[11] * z;
         out[12] = a[12];
@@ -255,55 +214,23 @@ class Mat4 {
         return out;
     }
     static multiply(a, b) {
-        var a00 = a[0],
-            a01 = a[1],
-            a02 = a[2],
-            a03 = a[3];
-        var a10 = a[4],
-            a11 = a[5],
-            a12 = a[6],
-            a13 = a[7];
-        var a20 = a[8],
-            a21 = a[9],
-            a22 = a[10],
-            a23 = a[11];
-        var a30 = a[12],
-            a31 = a[13],
-            a32 = a[14],
-            a33 = a[15]; // Cache only the current line of the second matrix
-        var b0 = b[0],
-            b1 = b[1],
-            b2 = b[2],
-            b3 = b[3];
         let out = new Float32Array(16);
-        out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-        out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-        out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-        out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-        b0 = b[4];
-        b1 = b[5];
-        b2 = b[6];
-        b3 = b[7];
-        out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-        out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-        out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-        out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-        b0 = b[8];
-        b1 = b[9];
-        b2 = b[10];
-        b3 = b[11];
-        out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-        out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-        out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-        out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
-        b0 = b[12];
-        b1 = b[13];
-        b2 = b[14];
-        b3 = b[15];
-        out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
-        out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
-        out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
-        out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+        out[0] =  b[0]  * a[0] + b[1]  * a[4] + b[2]  * a[8]  + b[3]  * a[12];
+        out[1] =  b[0]  * a[1] + b[1]  * a[5] + b[2]  * a[9]  + b[3]  * a[13];
+        out[2] =  b[0]  * a[2] + b[1]  * a[6] + b[2]  * a[10] + b[3]  * a[14];
+        out[3] =  b[0]  * a[3] + b[1]  * a[7] + b[2]  * a[11] + b[3]  * a[15];
+        out[4] =  b[4]  * a[0] + b[5]  * a[4] + b[6]  * a[8]  + b[7]  * a[12];
+        out[5] =  b[4]  * a[1] + b[5]  * a[5] + b[6]  * a[9]  + b[7]  * a[13];
+        out[6] =  b[4]  * a[2] + b[5]  * a[6] + b[6]  * a[10] + b[7]  * a[14];
+        out[7] =  b[4]  * a[3] + b[5]  * a[7] + b[6]  * a[11] + b[7]  * a[15];
+        out[8] =  b[8]  * a[0] + b[9]  * a[4] + b[10] * a[8]  + b[11] * a[12];
+        out[9] =  b[8]  * a[1] + b[9]  * a[5] + b[10] * a[9]  + b[11] * a[13];
+        out[10] = b[8]  * a[2] + b[9]  * a[6] + b[10] * a[10] + b[11] * a[14];
+        out[11] = b[8]  * a[3] + b[9]  * a[7] + b[10] * a[11] + b[11] * a[15];
+        out[12] = b[12] * a[0] + b[13] * a[4] + b[14] * a[8]  + b[15] * a[12];
+        out[13] = b[12] * a[1] + b[13] * a[5] + b[14] * a[9]  + b[15] * a[13];
+        out[14] = b[12] * a[2] + b[13] * a[6] + b[14] * a[10] + b[15] * a[14];
+        out[15] = b[12] * a[3] + b[13] * a[7] + b[14] * a[11] + b[15] * a[15];
         return out;
     }
     static lookAt(eye, center, up) {
